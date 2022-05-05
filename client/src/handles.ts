b02731ae2865383f53b2c7778d06e26ad783f8c1
Олег Иванов <olegivanov@Oleg-MacBook-Pro.local>
2022-05-05 18:32:06 +0400

    add new api root

--- client/src/handles.ts
+++ client/src/handles.ts
@@ -1,22 +1,27 @@
 import { IDocumentInfo } from "./interfaces";
 
+const apiRoot =
+  process.env.NODE_ENV === "production"
+    ? "https://motorsland-api.azurewebsites.net"
+    : "http://localhost:3001";
+
 export const getDocumentsInfo = async (): Promise<IDocumentInfo[]> => {
-  const resp = await fetch("http://localhost:3001/documents");
+  const resp = await fetch(apiRoot + "/documents");
 
   return (await resp.json()).documents;
 };
 export const getNewItemsCount = async (): Promise<number> => {
-  const resp = await fetch("http://localhost:3001/items/count");
+  const resp = await fetch("/items/count");
 
   return (await resp.json()).newItemsCount;
 };
 export const getPublicUrl = async (name: string): Promise<string> => {
-  const resp = await fetch(`http://localhost:3001/documents/${name}/url`);
+  const resp = await fetch(apiRoot + `/documents/${name}/url`);
 
   return (await resp.json()).publicUrl;
 };
 export const downloadDocument = async (name: string): Promise<void> => {
-  const response = await fetch(`http://localhost:3001/documents/${name}`);
+  const response = await fetch(apiRoot + `/documents/${name}`);
   const buffer = await response.blob();
 
   const a = document.createElement("a");
@@ -30,14 +35,14 @@ export const downloadDocument = async (name: string): Promise<void> => {
 export const deleteDocument = async (
   name: string
 ): Promise<IDocumentInfo[]> => {
-  const resp = await fetch(`http://localhost:3001/documents/${name}`, {
+  const resp = await fetch(apiRoot + `/documents/${name}`, {
     method: "DELETE",
   });
 
   return (await resp.json()).documents;
 };
 export const createUploadingDocument = async (): Promise<IDocumentInfo[]> => {
-  const resp = await fetch(`http://localhost:3001/documents`, {
+  const resp = await fetch(apiRoot + `/documents`, {
     method: "POST",
   });
 
