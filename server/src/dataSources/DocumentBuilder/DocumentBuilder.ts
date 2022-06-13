5415d9e56f91f1c1f55493cb912d905e909106da
Oleg Ivanov <oleg.ivanov@kapitalbank.az>
2022-06-14 01:43:23 +0400

    try to setup Puppeteer

--- server/src/dataSources/DocumentBuilder/DocumentBuilder.ts
+++ server/src/dataSources/DocumentBuilder/DocumentBuilder.ts
@@ -26,6 +26,10 @@ export class DocumentBuilder implements IDocumentBuilder {
     this.browser = await Puppeteer.launch({
       headless: true,
       defaultViewport: null,
+      args:
+        process.env.NODE_ENV === "production"
+          ? ["--use-gl=egl", "--no-sandbox", "--disable-setuid-sandbox"]
+          : [],
     });
   }
 
